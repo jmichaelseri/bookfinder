fce3d0b7e65368e9b6e914d26843830698f85a1e
jmichaelseri <juniormichaesleri@protonmail.com>
2021-11-04 16:01:14 +0000

    built header component with tailwind styles

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className="py-2 md:py-4 bg-green-600 w-auto  md:w-3/4 mx-auto rounded">
+            <h1 className="text-center text-2xl font-bold text-white">Book Finder</h1>
+        </header>
+    )
+}
+
+export default Header
