85b8bcebc0908ec5b27e9649c05fd3963d177cd0
jmichaelseri <juniormichaesleri@protonmail.com>
2021-11-04 15:59:30 +0000

    built button component with tailwind styles

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Button = () => {
+const Button = props => {
     return (
-        <div>
-            
-        </div>
+        <button type="submit" className="text-white rounded bg-green-600 text-center font-bold py-1 px-6 my-3 hover:bg-green-500 hover:text-white transition duration-250 col-span-1 col-start-2">
+            {props.children}
+            </button>
     )
 }
 
